7721497fa4f11d2034383bdd91fe51391f0e553f
Yurdiansyah <yurdi@griffin>
2022-06-20 00:50:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center : true,
+      padding:'16px'
+    },
+    extend: {
+      colors: {
+        primary : "#047857"
+      },
+      screen: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
